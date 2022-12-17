92d256c77d910c8d2a8a556ac7d46195ecbc6346
iancarpenter <ianhcarpenter@gmail.com>
2022-12-17 23:04:38 +0000

    Add project files.

---
+++ CsvReader/FordEscort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvReader
+{
+    public class FordEscort
+    {  
+        public FordEscort(string year, string mileage, string price)
+        {
+            Year = year;
+            Mileage = mileage;
+            Price = price;
+        }
+            
+        public string Year { get; set; }
+
+        public string Mileage { get; set; }
+
+        public string Price { get; set; }
+    }
+}
---
+++ CsvReader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace CsvReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            using (var reader = new StreamReader(@"C:\Temp\ford_escort.csv"))
+            {
+                List<FordEscort> fordEscorts = new List<FordEscort>();
+
+                string headerLine = reader.ReadLine();
+                while(!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    var csValues = line.Split(',');
+
+                    fordEscorts.Add(new FordEscort(csValues[0].Trim(), csValues[1].Trim(), csValues[2].Trim()));                    
+                }
+            }
+        }
+    }
+}
